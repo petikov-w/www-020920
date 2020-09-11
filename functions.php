4bdc43a5779d55437fee3c6ee9141e5add6d0a6f
Petikov Vladimir <vx7xv@inbox.ru>
2020-09-11 18:18:59 +0300

    Добавлены все файлы проекта по состоянию на 11.09.20

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+// функция форматирования телефонного номера
+
+function formatTelefonNumber($telefon) {
+    $temp = $telefon;
+    if ($temp[0]=='8') { $pref=substr($temp,0,1);}
+    elseif($temp[0]=='+') {$pref=substr($temp,0,2);}
+
+    $temp = substr($temp, -10);
+    $temp=$pref.'&nbsp&nbsp('.substr($temp, 0,3).')&nbsp&nbsp'.substr($temp, 3,3).'&nbsp'.substr($temp, 6,2).
+        '&nbsp'.substr($temp, 8,2);
+    $telefon = $temp;
+    return $telefon;
+}
---
+++ variables.php
@@ -0,0 +1,7 @@
+<?php
+    namespace vars;
+
+    $open_time = '12-00';
+    $close_time = '24-00';
+    $telefon = '89094282463';
+
